186acf53343c7b5df72e050c5f459ed83af02a54
Eddie-Pak <foalstn@naver.com>
2025-12-24 02:31:46 +0900

    feat: Banner Card 구현

---
+++ presentation/src/main/java/com/fastcampusmall/presentation/ui/component/BannerListCard.kt
@@ -0,0 +1,68 @@
+package com.fastcampusmall.presentation.ui.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.fastcampusmall.domain.model.BannerList
+import com.fastcampusmall.presentation.R
+import kotlinx.coroutines.delay
+
+@Composable
+fun BannerListCard(model: BannerList) {
+    val pagerState = rememberPagerState(pageCount = { model.imageList.size })
+
+    LaunchedEffect(pagerState) {
+        autoScrollInfinity(pagerState)
+    }
+
+    HorizontalPager(state = pagerState) {
+        Card(
+            shape = RoundedCornerShape(12.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+                .shadow(20.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.product_image),
+                contentDescription = "description",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxWidth()
+                    .aspectRatio(2f)
+            )
+
+            Box(
+                contentAlignment = Alignment.TopEnd,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("pageNumber : $it")
+            }
+        }
+    }
+}
+
+private suspend fun autoScrollInfinity(pagerState: PagerState) {
+    while (true) {
+        delay(3000)
+        val currentPage = pagerState.currentPage
+        var nextPage = currentPage + 1
+        if (nextPage >= pagerState.pageCount) nextPage = 0
+        pagerState.animateScrollToPage(nextPage)
+    }
+}
