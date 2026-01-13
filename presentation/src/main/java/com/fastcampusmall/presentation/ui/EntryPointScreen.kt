fabdbf3d1e054cd70177c84c6fa06d386e3b8ee7
Eddie-Pak <foalstn@naver.com>
2026-01-14 02:50:54 +0900

    feat: BottomBar 조건문 구현

--- presentation/src/main/java/com/fastcampusmall/presentation/ui/EntryPointScreen.kt
+++ presentation/src/main/java/com/fastcampusmall/presentation/ui/EntryPointScreen.kt
@@ -5,21 +5,41 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.fastcampusmall.presentation.common.CustomAppBar
 import com.fastcampusmall.presentation.navigation.AppNavHost
 import com.fastcampusmall.presentation.common.CustomNavigationBar
+import com.fastcampusmall.presentation.navigation.ScreenRouteDef
 
 @Composable
 fun EntryPointScreen() {
     val snackBarHostState = remember { SnackbarHostState() }
     val navController = rememberNavController()
 
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
+    val showBottomBarRoutes = listOf(
+        ScreenRouteDef.Main::class.qualifiedName,
+        ScreenRouteDef.CategoryMain::class.qualifiedName,
+        ScreenRouteDef.MyPage::class.qualifiedName
+    )
+
+    val isShowBottomBar = showBottomBarRoutes.any { route ->
+        currentDestination?.route?.contains(route ?: "") == true
+    }
+
     Scaffold(
         snackbarHost = { snackBarHostState },
         topBar = { CustomAppBar() },
-        bottomBar = { CustomNavigationBar(navController) }
+        bottomBar = {
+            if (isShowBottomBar) {
+                CustomNavigationBar(navController)
+            }
+        }
     ) { innerPadding ->
         AppNavHost(
             navController = navController,
